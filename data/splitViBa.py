f52e7e489fe13a3b6484527810552af73975b1a7
Bui Ngo Hoang Long <buingohoanglong2000@gmail.com>
2021-12-08 08:58:47 +0700

    split vi, ba; add accent

--- data/splitViBa.py
+++ data/splitViBa.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
-dir = './Dữ liệu cũ (Đài phát thanh Vĩnh Thạnh)/VH-TT'
+dir = '.'
 
-df = pd.read_csv(f'{dir}/VH-TT.csv')
+df = pd.read_csv(f'{dir}/train-synonymaugment.csv').sample(frac=1)
 vi = list(df.iloc[:,0])
 ba = list(df.iloc[:,1])
 
-with open(f'{dir}/VH-TT.vi', mode='w+', encoding='utf-8') as file_vi:
-    with open(f'{dir}/VH-TT.ba', mode='w+', encoding='utf-8') as file_ba:
+with open(f'{dir}/train-synonymaugment.vi', mode='w+', encoding='utf-8') as file_vi:
+    with open(f'{dir}/train-synonymaugment.ba', mode='w+', encoding='utf-8') as file_ba:
         for i in range(len(vi)):
             file_vi.write(vi[i] + '\n')
             file_ba.write(ba[i] + '\n')
